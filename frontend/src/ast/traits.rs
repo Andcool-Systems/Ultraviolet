77557f225e99757e773071cbaafbd7c1c2854c43
Andcool-Systems <drolll2007@gmail.com>
2026-02-27 23:01:28 +0300

    feat(traits): implement IsCompatible trait and update GetType method signature

--- frontend/src/ast/traits.rs
+++ frontend/src/ast/traits.rs
@@ -2,5 +2,11 @@ use crate::ast::types::UVType;
 
 pub trait GetType {
     /// Get type of node / value
-    fn get_type(&self) -> UVType;
+    /// TODO: Replace scope with real scope type
+    fn get_type(&self, scope: Option<usize>) -> UVType;
+}
+
+pub trait IsCompatible {
+    /// Check if left (current) type is compatible with right type
+    fn is_compatible(&self, other: &UVType) -> bool;
 }
--- frontend/src/ast/types.rs
+++ frontend/src/ast/types.rs
@@ -1,4 +1,6 @@
-use crate::ast::traits::GetType;
+use std::sync::mpsc::Sender;
+
+use crate::ast::traits::{GetType, IsCompatible};
 
 /// Typed value container
 pub enum UVValue {
@@ -10,7 +12,7 @@ pub enum UVValue {
 }
 
 impl GetType for UVValue {
-    fn get_type(&self) -> UVType {
+    fn get_type(&self, _scope: Option<usize>) -> UVType {
         match self {
             UVValue::Int(_) => UVType::Int,
             UVValue::Float(_) => UVType::Float,
@@ -22,6 +24,7 @@ impl GetType for UVValue {
 }
 
 /// Ultraviolet primitive types
+#[derive(PartialEq)]
 pub enum UVType {
     Int,
     Float,
@@ -29,5 +32,39 @@ pub enum UVType {
     Boolean,
     Null,
 
-    Union(Vec<UVValue>),
+    Union(Vec<UVType>),
+}
+
+impl IsCompatible for UVType {
+    fn is_compatible(&self, other: &UVType) -> bool {
+        if self == other {
+            return true;
+        }
+
+        match (self, other) {
+            (_, UVType::Union(types)) => types.iter().any(|t| self.is_compatible(t)),
+            (UVType::Union(types), _) => types.iter().any(|t| t.is_compatible(other)),
+
+            _ => false,
+        }
+    }
+}
+
+// ---------------
+pub enum Symbol {
+    /// Primitive type
+    Primitive(UVValue),
+
+    /// Name of the variable in scope
+    Variable(String),
+}
+
+impl GetType for Symbol {
+    fn get_type(&self, scope: Option<usize>) -> UVType {
+        match self {
+            Self::Primitive(val) => val.get_type(None),
+            // Scope-based search of the final primitive
+            Self::Variable(var) => todo!(),
+        }
+    }
 }
